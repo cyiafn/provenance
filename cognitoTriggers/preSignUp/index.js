8a61c0f9ff1a1705213b0c9b8fa7f36706032865
yankai14 <limyk2014@gmail.com>
2022-02-28 12:43:44 +0800

    commit shit

--- cognitoTriggers/preSignUp/index.js
+++ cognitoTriggers/preSignUp/index.js
@@ -1,21 +1,33 @@
+const axios = require('axios');
+
 module.exports.handler = async event => {
-    console.log('Event: ', JSON.stringify(event, null, 2));
+  console.log('Event: ', JSON.stringify(event, null, 2));
   
-    try {
-      const { request = {} } = event;
-      const { userNotFound } = request;
-      if (userNotFound) {
-        throw new Error('[404] User Not Found');
-      }
-  
-      const nonce = Math.floor(Math.random() * 1000000).toString();
-      const message = `I am signing my one-time nonce: ${nonce}`;
-  
-      event.response.publicChallengeParameters = { message };
-      event.response.privateChallengeParameters = { message };
-  
-      return event;
-    } catch (err) {
-      throw err;
+  const body = JSON.parse(event.userName);
+
+  const config = {
+    headers: {
+      'x-api-key': 'YFrR9r5nSR2KkP5VMwhYwaQ6oV14bHjl1uZSytHo'
     }
+  }
+
+  const response = await axios.post('https://gollbqzbfc.execute-api.ap-southeast-1.amazonaws.com/prod/verifyWallet', {
+    "publicAddress": event.userName
+  }, config);
+
+  try {
+    const response = await axios.post('https://gollbqzbfc.execute-api.ap-southeast-1.amazonaws.com/prod/verifyWallet', {
+    "publicAddress": event.userName
+    }, config);
+    event.response.autoConfirmUser = true;
+    return event;
+  } catch(err) {
+    if (err.response) {
+      if (err.response.status === 404) {
+        throw new Error('[404] Invalid Wallet Account');
+      } else {
+        throw new Error('[500] Internal Server Error');
+      }
+    }
+  }
 };
